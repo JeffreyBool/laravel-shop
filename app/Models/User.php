09ec7a79037e3540e0f0185bb1ed74661c84e590
JeffreyBool <1402992668@qq.com>
2020-07-15 19:03:39 +0800

    收货地址列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,28 +12,36 @@ class User extends Authenticatable implements MustVerifyEmail
 
     /**
      * The attributes that are mass assignable.
-     *
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name',
+        'email',
+        'password',
     ];
 
     /**
      * The attributes that should be hidden for arrays.
-     *
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
+        'remember_token',
     ];
 
     /**
      * The attributes that should be cast to native types.
-     *
      * @var array
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function addresses()
+    {
+        return $this->hasMany(UserAddress::class);
+    }
 }
---
+++ app/Models/UserAddress.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserAddress extends Model
+{
+    protected $fillable = [
+        'province',
+        'city',
+        'district',
+        'address',
+        'zip',
+        'contact_name',
+        'contact_phone',
+        'last_used_at',
+    ];
+
+    protected $dates = ['last_used_at'];
+
+    /**
+     * @return Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * @return string
+     */
+    public function getFullAddressAttribute(): string
+    {
+        return "{$this->province}{$this->city}{$this->district}{$this->address}";
+    }
+}
