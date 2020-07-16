2f616c7ca37480aafdeb1d76f32c13b58496d358
JeffreyBool <1402992668@qq.com>
2020-07-16 20:48:15 +0800

    购物车页面

--- routes/web.php
+++ routes/web.php
@@ -31,5 +31,7 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
 
     Route::post('cart', 'CartController@add')->name('cart.add');
+    Route::get('cart', 'CartController@index')->name('cart.index');
+    Route::delete('cart/{sku}', 'CartController@remove')->name('cart.remove');
 });
 
