e3ae4897cf24e018526ad18265411d9d05a63049
JeffreyBool <1402992668@qq.com>
2020-07-16 19:31:18 +0800

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
-
 Auth::routes(['verify' => true]);
 
 Route::group(['middleware' => ['auth', 'verified']], function() {
@@ -26,3 +24,5 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
 });
 
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
